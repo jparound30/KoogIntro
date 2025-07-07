3c38bb637904e505f6bbea7a7232237bd96cc5a7
jparound30 <japanese.around30@gmail.com>
2025-07-07 23:44:15 +0900

    add: add koog simple sample code.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,43 @@
 package com.github.jparound30
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import ai.koog.agents.core.agent.AIAgent
+import ai.koog.agents.core.tools.ToolRegistry
+import ai.koog.agents.ext.tool.AskUser
+import ai.koog.agents.ext.tool.ExitTool
+import ai.koog.agents.ext.tool.SayToUser
+import ai.koog.agents.features.eventHandler.feature.handleEvents
+import ai.koog.prompt.executor.clients.google.GoogleModels
+import ai.koog.prompt.executor.llms.all.simpleGoogleAIExecutor
+import io.github.cdimascio.dotenv.dotenv
+import kotlinx.coroutines.runBlocking
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+val dotenv = dotenv()
+val key = dotenv["GOOGLE_AI_API_KEY"]
+val toolRegistry = ToolRegistry {
+    tool(SayToUser)
+    tool(AskUser)
+    tool(ExitTool)
+}
+
+@OptIn(ExperimentalUuidApi::class)
+val agent = AIAgent(
+    executor = simpleGoogleAIExecutor(key),
+    llmModel = GoogleModels.Gemini2_0Flash,
+    systemPrompt = "You are a helpful assistant. Answer user questions concisely.",
+    toolRegistry = toolRegistry,
+) {
+    handleEvents {
+        onAgentRunError { strategyName: String, sessionUuid: Uuid?, throwable: Throwable ->
+            println("Agent finished with result: ${throwable.message}")
+        }
+        onAgentFinished { strategyName: String, result: String? ->
+            println("Agent finished. result: $result")
+        }
     }
+}
+
+fun main() = runBlocking {
+    val result = agent.run("Hello! How can you help me?")
 }
